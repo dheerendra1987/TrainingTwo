bff69d0ecbef289d2277ff087ba89fc1441102a9
dheerendra <dheerendra@gmail.com>
2013-04-01 11:05:24 +0530

    This is first  commit for maven

---
+++ src/test/java/AppTest.java
@@ -0,0 +1,19 @@
+
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import junit.framework.TestCase;
+public class AppTest extends TestCase {
+	
+	public void setUp() throws Exception{
+		
+		super.setUp();
+	}
+protected void tearDown() throws Exception{
+	
+	super.tearDown();
+	
+}
+public void test(){}
+}
